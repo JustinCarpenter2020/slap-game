6ce26e6851dd19a447a5d82aede20710cd6c5237
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2020-12-10 14:53:09 -0700

    finished 1st version

--- app.js
+++ app.js
@@ -20,8 +20,17 @@ function alert(health){
 function update(){
  document.getElementById("health").innerText=health.toString()
  document.getElementById("hits").innerText=hits.toString()
+ gameOver()
 }
 
+function gameOver(){
+  if (health == 0) {
+    
+    document.getElementById("game-header").style.visibility = "hidden";
+    document.getElementById("game-over").innerText="Game Over"
+  }
+
+}
 
 function kick(){
   health -=10
@@ -37,4 +46,6 @@ function kick(){
     update()
     }
     
-   
+   function draw(){
+
+   }
